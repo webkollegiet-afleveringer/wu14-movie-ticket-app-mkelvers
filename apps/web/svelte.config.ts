099fe9d4b65cb6704774d31087adb9b04600cc63
mkelvers <mikkelelvers@outlook.com>
2026-04-20 13:03:04 +0200

    chore: small fixes

--- apps/web/svelte.config.ts
+++ apps/web/svelte.config.ts
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
+import type { Config } from '@sveltejs/kit';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+const config: Config = {
 	compilerOptions: {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
