67e823045d31bf3e17ccb4465e7120a376a4f0f2
Milas Holsting <git@milasholsting.dk>
2026-04-21 14:11:23 +0200

    add upcommming images to homepage

---
+++ apps/web/src/routes/(main)/+page.server.ts
@@ -0,0 +1,13 @@
+import TMDBClient from "$lib/server/tmdb/client";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ fetch }) => {
+  const upcoming = await TMDBClient.GET("/3/movie/upcoming",
+    {
+      fetch,
+    });
+
+  return {
+    upcomming: upcoming.data
+  };
+};
