d4ea9c603a5d0b88aabad5fa6b6e3b2b91688014
Milas Holsting <git@milasholsting.dk>
2026-04-21 10:31:31 +0200

    finish auth

--- apps/web/src/routes/login/+page.server.ts
+++ apps/web/src/routes/login/+page.server.ts
@@ -6,7 +6,7 @@ import { APIError } from 'better-auth/api';
 
 export const load: PageServerLoad = (event) => {
   if (event.locals.user) {
-    return redirect(302, '/demo/better-auth');
+    return redirect(302, '/');
   }
   return {};
 };
@@ -32,7 +32,7 @@ export const actions: Actions = {
       return fail(500, { message: 'Unexpected error' });
     }
 
-    return redirect(302, '/demo/better-auth');
+    return redirect(302, '/');
   },
   signUpEmail: async (event) => {
     const formData = await event.request.formData();
@@ -64,7 +64,7 @@ export const actions: Actions = {
       return fail(500, { message: 'Unexpected error' });
     }
 
-    return redirect(302, '/demo/better-auth');
+    return redirect(302, '/');
   },
   signUpSSO: async (event) => {
     const formData = await event.request.formData();
--- apps/web/src/routes/login/+page.svelte
+++ apps/web/src/routes/login/+page.svelte
@@ -16,8 +16,7 @@
 		/>
 	</label>
 	<label>
-		Password
-		<input
+		Password <input
 			type="password"
 			name="password"
 			class="mt-1 rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none"
