4d6d94eac228c2b79d346810ac3b372c7c50b179
Milas Holsting <git@milasholsting.dk>
2026-04-24 09:03:03 +0200

    add tables

--- apps/web/src/lib/server/db/auth.schema.ts
+++ apps/web/src/lib/server/db/auth.schema.ts
@@ -1,5 +1,6 @@
 import { relations, sql } from "drizzle-orm";
 import { sqliteTable, text, integer, index } from "drizzle-orm/sqlite-core";
+import { tickets } from "./schema";
 
 export const user = sqliteTable("user", {
   id: text("id").primaryKey(),
@@ -90,6 +91,7 @@ export const verification = sqliteTable(
 export const userRelations = relations(user, ({ many }) => ({
   sessions: many(session),
   accounts: many(account),
+  tickts: many(tickets)
 }));
 
 export const sessionRelations = relations(session, ({ one }) => ({
--- apps/web/src/lib/server/db/schema.ts
+++ apps/web/src/lib/server/db/schema.ts
@@ -1,1 +1,46 @@
+import { integer, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import { randomUUID } from "crypto"
+import { relations } from 'drizzle-orm';
+import { user } from './auth.schema';
+
+export const cinema = sqliteTable("cinema", {
+  id: integer().primaryKey({ autoIncrement: true }),
+  name: text(),
+  location: text(),
+  image: text(),
+  rating: text(),
+  opens: text(),
+  closes: text(),
+});
+
+export const tickets = sqliteTable("ticket", {
+  orderId: integer({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  id: text().$defaultFn(randomUUID),
+  timeSlot: integer({ mode: "timestamp" }).notNull(),
+  seats: text({ mode: "json" }).$type<string[]>().notNull(),
+  movieId: text().notNull(),
+  status: text({ enum: ["failed", "awaiting confirmation", "successful"] }).default("awaiting confirmation"),
+  cimemaId: integer(),
+  userId: text(),
+}, (t) => [
+  uniqueIndex("ticketId_idx").on(t.id)
+])
+
+export const ticketRelations = relations(tickets, ({ one }) => ({
+  cinema: one(cinema, {
+    fields: [tickets.cimemaId],
+    references: [cinema.id]
+  }),
+  user: one(user, {
+    fields: [tickets.userId],
+    references: [user.id]
+  })
+}));
+
+export const cinemaRelations = relations(cinema, ({ many }) => ({
+  tickets: many(tickets)
+}))
+
+
+
 export * from './auth.schema';
